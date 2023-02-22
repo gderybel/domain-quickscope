a00c726b30a1a3a89bf57d56222c1c0055ac7301
guillaumederybel <guillaume.derybel@protonmail.com>
2023-02-22 15:08:15 +0100

    Add screenshot saver

--- DomainInformations.py
+++ DomainInformations.py
@@ -1,5 +1,6 @@
 from requests import get, ReadTimeout
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 def get_domain_title(domain: str):
     """
@@ -12,3 +13,12 @@ def get_domain_title(domain: str):
     soup = BeautifulSoup(response.content, 'html.parser')
     title = soup.title.string.strip()
     return title
+
+def get_screenshot(url: str):
+    """
+        This function will take a screenshot from a given URL.
+    """
+    driver = webdriver.Firefox()
+    driver.get(url)
+    filename = '/'.join(url.split('/')[2:3])[:30]
+    driver.save_screenshot(f"{filename}.png")
