1efcb0e7fd08c01fe98c95ed0f0a3cd8cfb00b11
guillaumederybel <guillaume.derybel@protonmail.com>
2023-02-25 14:33:39 +0100

    Add execution time to output

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from DorksLinks import get_dorks_link
 from DomainInformations import get_domain_title, get_related_domain_names, get_domain_informations
 from Browser import Browser
 from ReportGenerator import report_generation
+from time import time
 
 domain = input('Enter a domain or a webpage to test (e.g. : chanel.com) : ')
+
+start = time()
 
 browser = Browser()
 
@@ -33,3 +36,4 @@ browser.kill()
 del browser, count, domain_object, all_domains
 
 report_generation(domains_object_list)
+print(f"\nThe program took {int(time() - start)} seconds to execute.")
